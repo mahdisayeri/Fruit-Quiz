694c9f24d51a068af9901bdc82668f959a31ab2d
mahdi sayeri <mahdisayeri@yahoo.com>
2019-04-21 19:17:11 +0430

    add anim file and arras file

--- app/src/main/java/ir/tokaterm/fruitequiz/SettingsActivityFragment.java
+++ app/src/main/java/ir/tokaterm/fruitequiz/SettingsActivityFragment.java
@@ -1,5 +1,6 @@
 package ir.tokaterm.fruitequiz;
 
+import android.preference.PreferenceFragment;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,14 +10,14 @@ import android.view.ViewGroup;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class SettingsActivityFragment extends Fragment {
+public class SettingsActivityFragment extends PreferenceFragment {
 
     public SettingsActivityFragment() {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
     }
 }
