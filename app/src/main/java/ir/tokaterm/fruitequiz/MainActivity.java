ec11eacd48bbc582cd787209e3527f60b410b735
mahdi sayeri <mahdisayeri@yahoo.com>
2019-04-22 10:54:49 +0430

    completion MAinActivity code and add listener for change setting

--- app/src/main/java/ir/tokaterm/fruitequiz/MainActivity.java
+++ app/src/main/java/ir/tokaterm/fruitequiz/MainActivity.java
@@ -1,8 +1,11 @@
 package ir.tokaterm.fruitequiz;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,8 +13,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String CHOICES="pref_NumberOfChoice";
+    public static final String REGIONS="pref_regionsToInclude";
+    private boolean phoneDivice=true;
+    private boolean preferencesChenged=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +31,57 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        PreferenceManager.setDefaultValues(this,R.xml.preferences,false);
+        SharedPreferences sharedPreferences=PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(preferencesChengedListener);
+        int screenSize=getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
+        if(screenSize==Configuration.SCREENLAYOUT_SIZE_LARGE || screenSize==Configuration.SCREENLAYOUT_SIZE_XLARGE){
+          phoneDivice=false;
+        }
+        if(phoneDivice){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(preferencesChenged){
+            MainActivityFragment quizFragment= (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+//            quizFragment.updateGuessRows(PreferenceManager.getDefaultSharedPreferences(this));
+//            quizFragment.updateRegions(PreferenceManager.getDefaultSharedPreferences(this));
+//            quizFragment.resetQuiz();
+            preferencesChenged =false;
+        }
+    }
+
+
+    private SharedPreferences.OnSharedPreferenceChangeListener preferencesChengedListener=new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            preferencesChenged=true;
+            MainActivityFragment quizFragment= (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+            if(key.equals(CHOICES)){
+//                quizFragment.updateGuessRows(sharedPreferences);
+//                quizFragment.resetQuiz();
+            }else if(key.equals(REGIONS)){
+                Set<String> regions=sharedPreferences.getStringSet(REGIONS,null);
+                if(regions!=null && regions.size()>0){
+//                    quizFragment.updateRegions(sharedPreferences);
+//                    quizFragment.resetQuiz();
+                }else{
+                    regions.add(getString(R.string.default_region));
+                    SharedPreferences.Editor editor=sharedPreferences.edit();
+                    editor.putStringSet(REGIONS,regions);
+                    editor.apply();
+                    Toast.makeText(MainActivity.this, R.string.default_region_message, Toast.LENGTH_SHORT).show();
+                }
+            }
+            Toast.makeText(MainActivity.this, R.string.restarting_quiz, Toast.LENGTH_SHORT).show();
+        }
+    };
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
